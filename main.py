0963f0d97b9fc09cb9949058b833d22272ee14b5
Михаил Цой <mt6654747@gmail.com>
2023-05-04 20:10:20 +0900

    test over change windows

--- main.py
+++ main.py
@@ -97,3 +97,6 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
+
+
+#ТЕСТИМ ТЕСТИМ ТЕСТИМ 
