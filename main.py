6c401a23d15ab086bc67476708486dad49d9ec77
Михаил Цой <mt6654747@gmail.com>
2023-05-11 16:16:23 +0900

    add working version without classes

--- main.py
+++ main.py
@@ -97,6 +97,3 @@ if __name__ == '__main__':
     print('Воитель, Маг, Лекарь')
     char_class: str = choice_char_class()
     print(start_training(char_name, char_class))
-
-
-#ТЕСТИМ ТЕСТИМ ТЕСТИМ 
